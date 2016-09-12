3bfac4a8c9b899dfe260a630858df4407254d3dc
youanan <youanan@163.com>
2016-09-12 22:42:37 +0800

    基本布局完成,能运行,输出helloworld

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from views import bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    register_blueprints(app)
+
+    return app
+
+
+def register_blueprints(app):
+    app.register_blueprint(bp)
+
+
+
---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+bp = Blueprint('blog', __name__)
+
+
+@bp.route('/')
+def index():
+    return "Hello World"
+
+
