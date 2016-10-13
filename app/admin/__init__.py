37a9f127cbd6bdb5362c6dbd316bbf4e5545411d
youanan <youanan@163.com>
2016-10-13 22:01:08 +0800

    增加了flask-admin users视图

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,11 +1,13 @@
 from flask_admin import Admin
-from view import MyIndexView
+from view import MyIndexView, UserView
+from app.models import User
 
 
 def create_admin(app=None):
-    admin = Admin(app, name="iBlogAdmin",
+    admin = Admin(app,
+                  name="iBlogAdmin",
                   index_view=MyIndexView(),
                   base_template='admin/my_master.html')
-
+    admin.add_view(UserView(User,endpoint='users',name="UserList"))
 
 
--- app/admin/view.py
+++ app/admin/view.py
@@ -1,8 +1,13 @@
+# -*- coding:utf-8 -*-
 from flask_admin import AdminIndexView, expose
-
+from flask_admin.contrib.mongoengine import ModelView
 
 class MyIndexView(AdminIndexView):
-
+    '创建自定义视图'
     @expose('/')
     def index(self):
-        return super(MyIndexView, self).index()
+        #return super(MyIndexView, self).index()
+        return self.render('admin/my_master.html')
+
+class UserView(ModelView):
+    pass
