4af81e8e327c32324e4443589f7d1218eefce024
youanan <youanan@163.com>
2016-10-22 15:08:46 +0800

    babe扩展设置完成,日期格式改变

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,30 @@ from views import bp
 from models import db
 from app.admin import create_admin
 from flask.ext.login import LoginManager
+from filter import babel, my_format_datetime, format_meta_key
 
 
 def create_app():
-    # 定义一个工厂函数,被项目根目录run.py使用运行
     app = Flask(__name__)
-    # 创建Flask类实例
     app.config.from_object('config')
-    # 指定flask配置文件
+
+    register_babel(app)
+    register_jinjia_filters(app)
     register_blueprints(app)
-    # 注册蓝图
     register_database(app)
-    # 注册数据库
     create_admin(app)
-    # 注册flask-admin
     init_login(app)
-    # 注册flask-login
+
     return app
+
+
+def register_babel(app):
+    babel.init_app(app)
+
+
+def register_jinjia_filters(app):
+    app.jinja_env.filters['my_format_datetime'] = my_format_datetime
+    app.jinja_env.filters['format_meta_keywords'] = format_meta_key
 
 
 def register_blueprints(app):
---
+++ filter.py
@@ -0,0 +1,17 @@
+from flask_babel import Babel, format_datetime
+
+babel = Babel()
+
+
+def my_format_datetime(value, format='medium'):
+    if format == 'full':
+        format = "EEEE, d . MMMM . y 'at' HH:mm"
+    elif format == 'medium':
+        format = "EE dd/MM/y HH:mm"
+    return format_datetime(value, format)
+
+def format_meta_key(tags):
+    meta_keywords = 'flask blog'
+    if tags:
+        meta_keywords = ' '.join(tags).strip()
+    return meta_keywords
